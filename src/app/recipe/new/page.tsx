9158ad1e742d77821b8d3fdad9305bcbfd22577c
Alexander Painter <alexnpainter@gmail.com>
2023-09-28 10:13:20 +0100

    Add a bit of styling to import page

--- src/app/recipe/new/page.tsx
+++ src/app/recipe/new/page.tsx
@@ -3,13 +3,7 @@ import React from "react";
 import NewRecipeBody from "./NewRecipe";
 
 const NewRecipe = async () => {
-  const apiKey = process.env.OPENAI_API_KEY;
-
-  if (!apiKey) {
-    return <div>No api key present</div>;
-  }
-
-  return <NewRecipeBody openAIAPIKey={apiKey} />;
+  return <NewRecipeBody />;
 };
 
 export default NewRecipe;
