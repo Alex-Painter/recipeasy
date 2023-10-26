4406a85f9c38011783f2c593d3cac018b12378ff
Alexander Painter <alexnpainter@gmail.com>
2023-10-26 22:24:30 +0100

    Actually fix types this time

--- app/api/recipe/route.ts
+++ app/api/recipe/route.ts
@@ -2,18 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../lib/prisma";
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
+  // const body = await req.json();
 
-  let response;
-  if (body.name) {
-    response = await prisma.recipe.create({
-      data: { name: body.name },
-    });
-  }
+  // let response;
+  // if (body.name) {
+  //   response = await prisma.recipe.create({
+  //     data: { name: body.name },
+  //   });
+  // }
 
-  if (response) {
-    return NextResponse.json({ id: response.id });
-  }
+  // if (response) {
+  //   return NextResponse.json({ id: response.id });
+  // }
   return NextResponse.json({ error: true });
 }
 
