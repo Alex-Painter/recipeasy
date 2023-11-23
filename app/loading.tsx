37a0ac0dc0d3da89e030b391f97da906b0433d58
Alexander Painter <alexnpainter@gmail.com>
2023-11-23 18:00:16 +0000

    Remove top level loading

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-  return (
-    <div className="h-full w-full text-center align-middle mt-16">
-      <span className="loading loading-spinner text-warning mr-2"></span>
-    </div>
-  );
-};
-
-export default Loading;
