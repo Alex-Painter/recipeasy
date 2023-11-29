ecbc740539427ba73a24fcaafe58031142c96738
Alexander Painter <alexnpainter@gmail.com>
2023-11-29 22:57:08 +0000

    Add route specific metadata

--- app/create/[generationPromptId]/page.tsx
+++ app/create/[generationPromptId]/page.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import RecipeChat from "../../../components/GenerateRecipe/RecipeChat";
 import { getCurrentUser } from "../../../lib/session";
 import useChat from "../../../hooks/useChat";
+import { Metadata, ResolvingMetadata } from "next";
+import prisma from "../../../lib/prisma";
+import { metadata } from "../../layout";
 
-const PromptRecipes = async ({
-  params,
-}: {
+type PageProps = {
   params: { generationPromptId: string };
-}) => {
+};
+
+const PromptRecipes = async ({ params }: PageProps) => {
   const { generationPromptId } = params;
   const [chat, error] = await useChat(generationPromptId);
   const user = await getCurrentUser();
@@ -19,5 +22,40 @@ const PromptRecipes = async ({
     </div>
   );
 };
+
+export async function generateMetadata(
+  { params }: PageProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const id = params.generationPromptId;
+
+  const recipe = await prisma.recipe.findFirst({
+    where: {
+      promptId: id,
+    },
+    include: {
+      image: true,
+    },
+  });
+
+  if (!recipe || !recipe.image?.imageUrl) {
+    return metadata;
+  }
+
+  // optionally access and extend (rather than replace) parent metadata
+  const parentMetadata = await parent;
+  const previousImages = parentMetadata.openGraph?.images || [];
+
+  return {
+    ...metadata,
+    title: `${recipe?.name} | Omlete`,
+    openGraph: {
+      ...metadata.openGraph,
+      images: [recipe.image.imageUrl, ...previousImages],
+      title: `${recipe?.name} | Omlete`,
+      url: `https://eatomlete.com/create/${id}`,
+    },
+  };
+}
 
 export default PromptRecipes;
